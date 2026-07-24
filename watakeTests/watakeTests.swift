91fa2ebaca21243b6d9b2a4bbe58991af21d252b
reypryma <refanopradana@gmail.com>
2026-07-24 07:43:17 +0700

    feat: initial commit for watake_ios standalone repo

---
+++ watakeTests/watakeTests.swift
@@ -0,0 +1,19 @@
+//
+//  watakeTests.swift
+//  watakeTests
+//
+//  Created by mbairm3 on 23/07/26.
+//
+
+import Testing
+@testable import watake
+
+struct watakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
